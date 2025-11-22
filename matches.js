e0b8d7ecba3db96b932346a744aed13b5fc67aef
RushTownDarts <lindabarryrvd180@outlook.com>
2025-11-22 00:36:25 +0000

    Update matches.js

--- matches.js
+++ matches.js
@@ -1,6 +1,10 @@
 const matches = [
   { date: "14-11-2025", teamA: "Cilles B", scoreA: 7, teamB: "Rivervalley", scoreB: 1 },
   { date: "14-11-2025", teamA: "Round Towers", scoreA: 1, teamB: "Cilles A", scoreB: 7 },
-  { date: "14-11-2025", teamA: "Strand", scoreA: 6, teamB: "Top Shop", scoreB: 2 }
+  { date: "14-11-2025", teamA: "Strand", scoreA: 6, teamB: "Top Shop", scoreB: 2 },
+  { date: "21-11-2025", teamA: "Rivervalley", scoreA: 3, teamB: "Cilles A", scoreB: 5},
+  { date: "21-11-2025", teamA: "Top Shop", scoreA: 5, teamB: "Round Towers", scoreB: 3 },
+  { date: "21-11-2025", teamA: "Cricket", scoreA: 3, teamB: "Strand", scoreB: 3 }
+  
   
 ];
