01a297427b5480e057b57aabe5d6dc36679a352c
RushTownDarts <lindabarryrvd180@outlook.com>
2025-11-29 01:40:25 +0000

    Update 180s.js

--- 180s.js
+++ 180s.js
@@ -1,5 +1,6 @@
 const highs = [
     // Example template for when a player hits a 180:
     // { player: "Player Name", team: "Team Name", hit: 1 }
-    { player: "John Fitzgerald", team: "Cilles A", hit: 1 }
+    { player: "John Fitzgerald", team: "Cilles A", hit: 1 },
+    { player: "Carl Murphy", team: "Strand", hit: 1 }
 ];
