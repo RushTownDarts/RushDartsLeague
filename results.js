3574781ff0e03446d1669a8d945559da2513163d
RushTownDarts <lindabarryrvd180@outlook.com>
2026-01-31 01:04:13 +0000

    Update results.js

--- results.js
+++ results.js
@@ -1,11 +1,11 @@
 // results.js
 const results = [
-    { team: "Cilles A", won: 5, draw: 0, lost: 0 },
-    { team: "Cilles B", won: 3, draw: 0, lost: 2 },
-    { team: "Rivervalley", won: 0, draw: 1, lost: 4 },
-    { team: "Top Shop", won: 1, draw: 0, lost: 4},
+    { team: "Cilles A", won: 5, draw: 1, lost: 0 },
+    { team: "Cilles B", won: 3, draw: 1, lost: 2 },
+    { team: "Rivervalley", won: 0, draw: 2, lost: 4 },
+    { team: "Top Shop", won: 1, draw: 1, lost: 4},
     { team: "Strand", won: 5, draw: 0, lost: 1},
-     { team: "Cricket", won: 3, draw: 0, lost: 2},
-     { team: "Round Towers", won: 0, draw: 1, lost: 4 },
+     { team: "Cricket", won: 3, draw: 1, lost: 2},
+     { team: "Round Towers", won: 0, draw: 2, lost: 4 },
     
 ];
