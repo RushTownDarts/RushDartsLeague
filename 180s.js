2fc15ab0d98bbf40f82257da643a17e8d5e1dc02
RushTownDarts <lindabarryrvd180@outlook.com>
2026-04-08 13:16:59 +0100

    Update 180s.js

--- 180s.js
+++ 180s.js
@@ -2,6 +2,7 @@ const highs = [
     // Example template for when a player hits a 180:
     // { player: "Player Name", team: "Team Name", hit: 1 }
      { player: "John Fitzgerald", team: "Cilles A", hit: 3 },
+    { player: "Jacko", team: "Strand", hit: 2 },
     { player: "Anto Caldwell", team: "Cricket", hit: 2 },
     { player: "Carl Murphy", team: "Strand", hit: 1 },
      { player: "Nigel Donnelly", team: "Cilles A", hit: 1 },
@@ -10,6 +11,7 @@ const highs = [
     { player: "Derek Barry", team: "Cilles A", hit: 1 },
     { player: "Mairead Cahill", team: "Towers", hit: 1 },
     { player: "Ian Murphy", team: "Towers", hit: 1 },
+    { player: "Butsy", team: "Strand", hit: 1 },
     { player: "Aaron Lowndes", team: "Cilles A", hit: 1 },
     { player: "Will Lawor", team: "Cilles B", hit: 1 },
     { player: "Sean Wade", team: "Strand", hit: 1 },
